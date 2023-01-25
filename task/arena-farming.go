01003c98b03d018a4fe931fd5332803f9e0c7d30
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-01-25 17:19:26 +0300

    refactor: merge arena boosting into status

--- task/arena-farming.go
+++
@@ -1,6 +0,0 @@
-package task
-
-type ArenaFarmingConfig struct {
-	Enable        bool `json:"enable"`
-	UseEnergyCans bool `json:"use_energy_cans"`
-}
--- task/status.go
+++ task/status.go
@@ -1,5 +1,6 @@
 package task
 
 type Status struct {
-	ArenaFarming bool `json:"arena_farming"`
+	ArenaFarming       bool `json:"arena_farming"`
+	ArenaUseEnergyCans bool `json:"arena_use_energy_cans"`
 }
