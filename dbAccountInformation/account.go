68be2e4d37a4474db9488475e7223f459bf72590
Dmitriy Tikhomirov <uroborosq@ya.ru>
2023-01-27 11:04:24 +0300

    feat: add energy limit to DbAccountInformation

--- dbAccountInformation/account.go
+++ dbAccountInformation/account.go
@@ -1,9 +1,10 @@
 package dbAccountInformation
 
 type DbAccountInformation struct {
-	ID           int
-	GameID       int
-	FriendlyName string
-	Owner        int
-	URL          string
+	ID           int    `json:"id"`
+	GameID       int    `json:"game_id"`
+	FriendlyName string `json:"friendly_name"`
+	Owner        int    `json:"owner"`
+	URL          string `json:"url"`
+	EnergyLimit  int    `json:"energy_limit"`
 }
